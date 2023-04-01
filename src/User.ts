6a74d3abb68ebde52308f14a5578c661b0058fb7
Pepakayala Veera Ganesh Kumar <pepakayalaveeraganeshkumar@Pepakayalas-MacBook-Air.local>
2023-04-01 20:01:18 +0530

    added class of User

--- src/User.ts
+++ src/User.ts
@@ -1,4 +1,19 @@
-import { Faker } from "@faker-js/faker";
+import { faker } from '@faker-js/faker';
+
+export class User {
+    name!: string;
+    coordinate!: {
+        lat: number;
+        log: number;
+    };
 
 
-class User {}
+    constructor( ) {
+        this.name = faker.name.firstName('male'),
+        this.coordinate = {
+            lat : parseFloat(faker.address.latitude()),
+            log : parseFloat(faker.address.longitude()),
+        }
+     }
+
+}
